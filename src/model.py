ab1c914bfb0b7255d91329a187bbd7df1e68a557
Julius Olson <julius.olsons@gmail.com>
2020-04-28 15:46:48 +0200

    pretrained cnn model

--- src/model.py
+++ src/model.py
@@ -4,3 +4,13 @@ Definition of neural model.
 Consits of CNN and RNN.
 
 """
+import torchvision
+import torch
+import torch.nn as nn
+
+"""
+The CNN portion of the model 
+In the paper a pretrained vgg16 net is used.
+Available in pytorch.
+"""
+cnn = torchvision.models.vgg16(pretrained=True)
