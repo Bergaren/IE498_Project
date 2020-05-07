2eaad503a02e716bb26c584ca43d8ec6f7592091
Julius Olson <julius.olsons@gmail.com>
2020-05-07 17:25:45 +0200

    update config

--- src/config.py
+++ src/config.py
@@ -32,7 +32,7 @@ class Config(object):
 
 		# about the optimization
 		self.num_epochs = 100
-		self.batch_size = 4
+		self.batch_size = 32
 		self.optimizer = 'Adam'    # 'Adam', 'RMSProp', 'Momentum' or 'SGD'
 		self.initial_learning_rate = 0.0001
 		self.learning_rate_decay_factor = 1.0
@@ -65,8 +65,8 @@ class Config(object):
 		# about the evaluation
 		self.eval_image_dir = DATA_BASE_DIR + 'val/images/'
 		self.eval_caption_file = DATA_BASE_DIR + 'val/captions_val2014.json'
-		self.eval_result_dir = './val/results/'
-		self.eval_result_file = './val/results.json'
+		self.eval_result_dir = DATA_BASE_DIR + 'val/results/'
+		self.eval_result_file = DATA_BASE_DIR + 'val/results.json'
 		self.save_eval_result_as_image = False
 
 		# about the testing
