0170fab990c26f9fe25455644a28d089beb383b6
Julius Olson <julius.olsons@gmail.com>
2020-05-10 10:50:16 +0200

    add simple args

--- src/main.py
+++ src/main.py
@@ -8,14 +8,21 @@ from torch.utils.data import DataLoader
 from torch.autograd import Variable
 from utils.coco.pycocoevalcap.eval import COCOEvalCap
 import json
+from sys import argv
+
+if len(argv) < 3:
+	print("Supply arguments: load|new train|eval")
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 config = Config()
 
 
-#model = ImageCaptioner(config)
-#model = model.to(device)
-model = torch.load(config.save_dir)
+if argv[1] == "new":
+	model = ImageCaptioner(config)
+	model = model.to(device)
+else:
+	model = torch.load(config.save_dir)
+	
 
 train_data = prepare_train_data(config)
 coco, eval_data, vocabulary = prepare_eval_data(config)
@@ -28,71 +35,70 @@ train loop
 TODO: input arguments decide training/testing + model load
 """
 
+def train():
+	criterion = nn.CrossEntropyLoss()
+	optimizer = torch.optim.RMSprop(model.parameters(), lr = config.initial_learning_rate)
+	model.train()
+	for e in range(config.num_epochs):
+		hits = 0
+		for batch in tqdm(train_loader):
+			optimizer.zero_grad()
 
-criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.RMSprop(model.parameters(), lr = config.initial_learning_rate)
-model.train()
-for e in range(config.num_epochs):
-	hits = 0
-	for batch in tqdm(train_loader):
-		optimizer.zero_grad()
+			images = batch["images"]
+			images = Variable(torch.FloatTensor(images.float()))
+			images = images.permute(0,3,1,2)
+			images = images.to(device)
 
+			captions = batch["captions"]
+			captions = Variable(torch.LongTensor(captions.long())).to(device)
+			scores = model(images, captions)
+
+			loss = criterion(scores.permute(1, 2, 0), captions)
+			
+			loss.backward()
+			torch.nn.utils.clip_grad_norm_(model.parameters(), config.clip_gradients)
+
+			optimizer.step()
+
+			masks = batch["masks"]
+			pred = torch.argmax(scores, dim  = 2)
+			hits += pred.permute(1, 0).eq(captions).int().sum()
+		
+		print("Epoch %d achieved training accuracy of: %f" % (e+1,hits.item()*100.0/len(train_loader)))
+		# SAVE MODEL
+		
+		if (e+1) % config.save_period == 0:
+			torch.save(model, config.save_dir)
+
+
+def evaulate():
+	model.eval()
+	results = []
+	for batch in tqdm(test_loader):
 		images = batch["images"]
 		images = Variable(torch.FloatTensor(images.float()))
 		images = images.permute(0,3,1,2)
 		images = images.to(device)
-
-		captions = batch["captions"]
-		captions = Variable(torch.LongTensor(captions.long())).to(device)
-		scores = model(images, captions)
-
-		loss = criterion(scores.permute(1, 2, 0), captions)
-		
-		loss.backward()
-		torch.nn.utils.clip_grad_norm_(model.parameters(), config.clip_gradients)
-
-		optimizer.step()
-
-		masks = batch["masks"]
-		pred = torch.argmax(scores, dim  = 2)
-		hits += pred.permute(1, 0).eq(captions).int().sum()
-	
-	print("Epoch %d achieved training accuracy of: %f" % (e+1,hits.item()*100.0/len(train_loader)))
-	# SAVE MODEL
-	
-	if (e+1) % config.save_period == 0:
-		torch.save(model, config.save_dir)
-
-
-"""	
-model.eval()
-results = []
-for batch in tqdm(test_loader):
-	images = batch["images"]
-	images = Variable(torch.FloatTensor(images.float()))
-	images = images.permute(0,3,1,2)
-	images = images.to(device)
-		
-	pred = model(images, captions = None)
-		
-	for i in range(pred.shape[1]):
-		results.append({
-			"caption": str(vocabulary.get_sentence(pred[:, i])), 
-			"image_id": int(batch["image_ids"][i].item())
-		})
-
-# Write generated captions to result file		
-with open(config.eval_result_file, "w") as resfile:
-	json.dump(results, resfile)
-		
-# Evaluate these captions
-eval_result_coco = coco.loadRes(config.eval_result_file)
-scorer = COCOEvalCap(coco, eval_result_coco)
-scorer.evaluate()
-print("Evaluation complete.")
-"""
-		
-
-
-
 			
+		pred = model(images, captions = None)
+			
+		for i in range(pred.shape[1]):
+			results.append({
+				"caption": str(vocabulary.get_sentence(pred[:, i])), 
+				"image_id": int(batch["image_ids"][i].item())
+			})
+
+	# Write generated captions to result file		
+	with open(config.eval_result_file, "w") as resfile:
+		json.dump(results, resfile)
+			
+	# Evaluate these captions
+	eval_result_coco = coco.loadRes(config.eval_result_file)
+	scorer = COCOEvalCap(coco, eval_result_coco)
+	scorer.evaluate()
+	print("Evaluation complete.")
+		
+if argv[2] == "eval":
+	evaulate()
+else:
+	train()
